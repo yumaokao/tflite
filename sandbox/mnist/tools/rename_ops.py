caca9c300c66118580c77194dbbc356a3a5367de
yumaokao <ymkq9h@gmail.com>
2017-12-13 15:10:48 +0800

    add rename_by_ids to rename_ops.py

--- sandbox/mnist/tools/rename_ops.py
+++ sandbox/mnist/tools/rename_ops.py
@@ -22,25 +22,20 @@ def display_nodes(nodes):
             print('  - {} {}'.format(i, n))
 
 
-def find_dropouts(nodes):
-    nodes = filter(lambda n: len(n.name.split('/')) > 1, nodes)
-    # find dropout/dropout/add Add
-    dropns = filter(lambda n: n.name.split('/')[-2].startswith('dropout'), nodes)
-    dropns = filter(lambda n: n.name.split('/')[-1] == 'add', dropns)
-    drops = map(lambda n: '/'.join(n.name.split('/')[:-1]), dropns)
-    # display_nodes(dropns)
-    # print(drops)
-    return drops
-
-
-def rename_nodes(graph_def, renames):
+def rename_nodes(graph_def, renames, reids):
     newnodes = graph_def.node[:]
     # display_nodes(newnodes)
-    for r in renames:
-        rp = r.split(',')
-        for n in newnodes:
-            if n.op == rp[0]:
-                n.op = rp[1]
+    if renames is not None:
+        for r in renames:
+            rp = r.split(',')
+            for n in newnodes:
+                if n.op == rp[0]:
+                    n.op = rp[1]
+
+    if reids is not None:
+        for r in reids:
+            rp = r.split(',')
+            newnodes[int(rp[0])].op = rp[1]
 
     # output graph
     display_nodes(newnodes)
@@ -52,24 +47,38 @@ def rename_nodes(graph_def, renames):
 def main():
     parser = argparse.ArgumentParser(description='rename ops')
     parser.add_argument('frozen_pbs', nargs='+', help='Frozen graph files (.pb)')
-    parser.add_argument('-r', '--renames', nargs='+',
-                        help='Ops to be renamed (ORIGIN_OP,NEW_OP)')
+    parser.add_argument('-n', '--rename_by_names', nargs='+',
+                        help='Ops to be renamed (ORIGIN_OP_NAME,NEW_OP_NAME)')
+    parser.add_argument('-i', '--rename_by_ids', nargs='+',
+                        help='Ops to be renamed (index,NEW_OP_NAME)')
     parser.add_argument('-v', '--version', action='version',
                         version=VERSION, help='show version infomation')
     args = parser.parse_args()
-    if args.renames is None:
-        parser.print_help()
-        sys.exit(1)
-    if len(filter(lambda r: len(r.split(',')) != 2, args.renames)):
-        parser.print_help()
-        sys.exit(1)
+    if args.rename_by_names is None and args.rename_by_ids is None:
+        # if not provide any rename rules, just display_nodes
+        for pb in args.frozen_pbs:
+            print('display all nodes of {}'.format(pb))
+            with tf.gfile.GFile(pb, "rb") as f:
+                graph_def = tf.GraphDef()
+                graph_def.ParseFromString(f.read())
+                display_nodes(graph_def.node)
+        sys.exit(0)
+
+    if args.rename_by_names is not None:
+        if len(filter(lambda r: len(r.split(',')) != 2, args.rename_by_names)):
+            parser.print_help()
+
+    if args.rename_by_ids is not None:
+        if len(filter(lambda r: len(r.split(',')) != 2, args.rename_by_ids)):
+            parser.print_help()
+            sys.exit(1)
 
     for pb in args.frozen_pbs:
         npb = '{}-custom{}'.format(*os.path.splitext(pb))
         with tf.gfile.GFile(pb, "rb") as f:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
-        n_graph_def = rename_nodes(graph_def, args.renames)
+        n_graph_def = rename_nodes(graph_def, args.rename_by_names, args.rename_by_ids)
         with tf.gfile.GFile(npb, "wb") as f:
             f.write(n_graph_def.SerializeToString())
 
