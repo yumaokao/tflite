05a4ea20912ecfd6130f9673e1ef1a1c0d0d6033
yumaokao <ymkq9h@gmail.com>
2017-12-18 20:36:07 +0800

    mnist: lstm remove static_rnn, makes thing simpiler first

--- sandbox/mnist/lstm/recurrent_network.py
+++ sandbox/mnist/lstm/recurrent_network.py
@@ -30,17 +30,17 @@ handle 28 sequences of 28 steps for every sample.
 # Training Parameters
 learning_rate = 0.001
 training_steps = 10000
-batch_size = 128
+batch_size = 64
 display_step = 200
 
 # Network Parameters
-num_input = 28 # MNIST data input (img shape: 28*28)
-timesteps = 28 # timesteps
+num_input = 784 # MNIST data input (img shape: 28*28)
+timesteps = 1 # timesteps
 num_hidden = 128 # hidden layer num of features
 num_classes = 10 # MNIST total classes (0-9 digits)
 
 # tf Graph input
-X = tf.placeholder("float", [None, timesteps, num_input])
+X = tf.placeholder("float", [None, num_input])
 Y = tf.placeholder("float", [None, num_classes])
 
 # Define weights
@@ -62,19 +62,20 @@ def RNN(x, weights, biases):
     # Required shape: 'timesteps' tensors list of shape (batch_size, n_input)
 
     # Unstack to get a list of 'timesteps' tensors of shape (batch_size, n_input)
-    x = tf.unstack(x, timesteps, 1)
+    # x = tf.unstack(x, timesteps, 1)
 
     # Define a lstm cell with tensorflow
     lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
 
     # Get initial_state
-    initial_state = lstm_cell.zero_state(batch_size, tf.float32)
+    state = lstm_cell.zero_state(batch_size, tf.float32)
 
     # Get lstm cell output
-    outputs, states = rnn.static_rnn(lstm_cell, x, initial_state=initial_state, dtype=tf.float32)
+    # outputs, states = rnn.static_rnn(lstm_cell, x, initial_state=initial_state, dtype=tf.float32)
+    output, state = lstm_cell(x, state)
 
     # Linear activation, using rnn inner loop last output
-    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+    return tf.matmul(output, weights['out']) + biases['out']
 
 logits = RNN(X, weights, biases)
 prediction = tf.nn.softmax(logits)
@@ -107,7 +108,7 @@ with tf.Session() as sess:
     for step in range(1, training_steps+1):
         batch_x, batch_y = mnist.train.next_batch(batch_size)
         # Reshape data to get 28 seq of 28 elements
-        batch_x = batch_x.reshape((batch_size, timesteps, num_input))
+        # batch_x = batch_x.reshape((batch_size, timesteps, num_input))
         # Run optimization op (backprop)
         sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
         if step % display_step == 0 or step == 1:
@@ -121,8 +122,9 @@ with tf.Session() as sess:
     print("Optimization Finished!")
 
     # Calculate accuracy for 128 mnist test images
-    test_len = 128
-    test_data = mnist.test.images[:test_len].reshape((-1, timesteps, num_input))
+    test_len = batch_size
+    # test_data = mnist.test.images[:test_len].reshape((-1, timesteps, num_input))
+    test_data = mnist.test.images[:test_len]
     test_label = mnist.test.labels[:test_len]
     print("Testing Accuracy:", \
         sess.run(accuracy, feed_dict={X: test_data, Y: test_label}))
