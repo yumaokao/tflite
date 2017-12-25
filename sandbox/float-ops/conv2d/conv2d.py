ad79050237cddd311e9ef5c1d50b1dcfb8cd683d
yumaokao <ymkq9h@gmail.com>
2017-12-25 10:28:38 +0800

    float-ops: add conv2d and depthwise_conv2d, fully_connected to add relu

---
+++ sandbox/float-ops/conv2d/conv2d.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+def model(x):
+  # return variables to save
+  variables = {}
+  x_2d = tf.reshape(x, [-1, 14, 14, 4])
+  W = weight_variable([3, 3, 4, 32], name='W')
+  b = bias_variable([32], name='b')
+  x_dconv2d = tf.nn.conv2d(x_2d, W, strides=[1, 1, 1, 1], padding='SAME')
+  y = tf.nn.relu(x_dconv2d + b, name='ys')
+  variables['W'] = W
+  variables['b'] = b
+  return y, variables
+
+def weight_variable(shape, name=None):
+  """weight_variable generates a weight variable of a given shape."""
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  pnames = {}
+  if name is not None:
+    pnames['name'] = name
+  return tf.Variable(initial, **pnames)
+
+def bias_variable(shape, name=None):
+  """bias_variable generates a bias variable of a given shape."""
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  pnames = {}
+  if name is not None:
+    pnames['name'] = name
+  return tf.Variable(initial, **pnames)
--- sandbox/float-ops/fully_connected/fully_connected.py
+++ sandbox/float-ops/fully_connected/fully_connected.py
@@ -7,7 +7,7 @@ def model(x):
   initial_b = tf.truncated_normal([10], stddev=0.1)
   W = tf.Variable(initial_W, name='W')
   b = tf.Variable(initial_b, name='b')
-  y = tf.add(tf.matmul(x, W), b, name='ys')
+  y = tf.nn.relu(tf.add(tf.matmul(x, W), b), name='ys')
   variables['W'] = W
   variables['b'] = b
   return y, variables
