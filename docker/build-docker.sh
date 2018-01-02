5b283f0ae524ab279ed02b1c111372e22e08b92a
yumaokao <ymkq9h@gmail.com>
2018-01-02 17:02:14 +0800

    update build-docker.sh

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -e -u
 
+DEVICE=${DEVICE:=CPU}
+TAG_NAME=latest-devel
+DOCKER=docker
+[ $DEVICE == "GPU" ] && TAG_NAME=latest-devel-gpu
+[ $DEVICE == "GPU" ] && DOCKER=nvidia-docker
 IMAGE_NAME=yumaokao/tensorflow/lite
 DOCKER_FILE=./Dockerfile
+
+echo "Building image '$IMAGE_NAME:$TAG_NAME'..."
+$DOCKER build -t "$IMAGE_NAME:$TAG_NAME" --build-arg TAG_NAME=$TAG_NAME --build-arg UID=$(id -u) .
 
 # CPU
 # TAG_NAME=latest-devel
@@ -10,6 +18,6 @@ DOCKER_FILE=./Dockerfile
 # docker build -t "$IMAGE_NAME:$TAG_NAME" --build-arg TAG_NAME=$TAG_NAME --build-arg UID=$(id -u) .
 
 # GPU
-TAG_NAME=latest-devel-gpu
-echo "Building image '$IMAGE_NAME:$TAG_NAME'..."
-nvidia-docker build -t "$IMAGE_NAME:$TAG_NAME" --build-arg TAG_NAME=$TAG_NAME --build-arg UID=$(id -u) .
+# TAG_NAME=latest-devel-gpu
+# echo "Building image '$IMAGE_NAME:$TAG_NAME'..."
+# nvidia-docker build -t "$IMAGE_NAME:$TAG_NAME" --build-arg TAG_NAME=$TAG_NAME --build-arg UID=$(id -u) .
