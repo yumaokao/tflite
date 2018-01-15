b5dc679ebba2c6bcc5343037385a2c2d43c68d05
wei-che.chang <wei-che.chang@mediatek.com>
2018-01-15 15:29:10 +0800

    call comparison with uint8 inference

--- sandbox/uint8-ops/concat/concat.py
+++ sandbox/uint8-ops/concat/concat.py
@@ -16,7 +16,7 @@ def model(x): # float-in, float-out
   W2 = tf.fake_quant_with_min_max_args(W, min=-1.0, max=3.0, num_bits=8)
  
   x_2d = tf.reshape(x, [-1, 28, 28, 1])
-  fake_x = tf.fake_quant_with_min_max_args(x_2d, min=-1.0, max=3.0, num_bits=8)
+  # fake_x = tf.fake_quant_with_min_max_args(x_2d, min=-1.0, max=3.0, num_bits=8)
 
   y = tf.concat([x_2d, W2], 1)
   y = tf.fake_quant_with_min_max_args(y, min=-1.0, max=3.0 ,name='ys')
