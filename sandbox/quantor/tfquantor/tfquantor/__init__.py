d8c182b069e3137422d4d7dc73a38da03534d5d0
yumaokao <ymkq9h@gmail.com>
2018-01-24 11:17:17 +0800

    tfquantor: initial a empty pip module tfquantor

---
+++ sandbox/quantor/tfquantor/tfquantor/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+def joke():
+  return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+        	u'Beiherhund das Oder die Flipperwaldt gersput.')
