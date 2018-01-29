e533b33dd1279b6dc2b2355aa4ace0ef96b1bbe2
yumaokao <ymkq9h@gmail.com>
2018-01-29 14:58:53 +0800

    tfquantor: add eval/bin/eval_tflite

--- sandbox/quantor/tfquantor/setup.py
+++ sandbox/quantor/tfquantor/setup.py
@@ -9,14 +9,14 @@ setup(
   author='yumaokao',
   author_email='yumao.kao@mediatek.com',
   description='Quantization module for tensorflow with FakeQuant*',
-  long_description="Quantization module for tensorflow and tflite with FakeQuant*, "
-                   "comes from tensorflow/contrib/quantize/ and "
-                   "https://arxiv.org/pdf/1712.05877",
+  long_description="Quantization module for tensorflow and tflite with "
+                   "FakeQuant* comes from tensorflow/contrib/quantize/ . "
+                   "Please also refer to https://arxiv.org/pdf/1712.05877",
   license='LICENSE.txt',
   url='http://www.github.com',
 
   packages=['tfquantor', 'tfquantor.quantize', 'tfquantor.eval'],
-  scripts=['tfquantor/eval/bin/eval_frozen'],
+  scripts=['tfquantor/eval/bin/eval_frozen', 'tfquantor/eval/bin/eval_tflite'],
   install_requires=[
     'tensorflow>=1.5.0rc1',
   ],
