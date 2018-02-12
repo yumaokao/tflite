f64de41a20ff4243b7e43bc7ed8f4e444af375ae
yumaokao <ymkq9h@gmail.com>
2018-02-12 14:52:55 +0800

    quantor: fold_batch_norms now needs to give is_training or not
    and update train_mobilenet_v1_quantize ckpts

--- sandbox/quantor/pre-quantor/train_image_classifier_quantize.py
+++ sandbox/quantor/pre-quantor/train_image_classifier_quantize.py
@@ -548,7 +548,9 @@ def main(_):
 
     # Quantize training graph
     g = tf.get_default_graph()
-    fold_batch_norms.FoldBatchNorms(g)
+    fold_batch_norms.FoldBatchNorms(g,
+                                    freeze_batch_norm_delay=0,
+                                    is_training=True)
     quantize.Quantize(g, is_training=True)
     for var in g.get_collection('variables'):
       if var.name.endswith('min:0') or var.name.endswith('max:0'):
