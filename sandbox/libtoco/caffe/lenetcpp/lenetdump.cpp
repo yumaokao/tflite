ab70141c0f1101e861973d256263b56b7f15791c
yumaokao <ymkq9h@gmail.com>
2018-02-20 07:45:19 +0800

    lenetcpp: lenetdump to parse convolution_param

--- sandbox/libtoco/caffe/lenetcpp/lenetdump.cpp
+++ sandbox/libtoco/caffe/lenetcpp/lenetdump.cpp
@@ -25,8 +25,24 @@ int main(int argc, char* argv[]) {
   cout << "network name : [" << network_name << "]" << endl;
   const vector<string>& layer_names = lenet.layer_names();
   cout << "network layers : " << layer_names.size() << endl;
-  for (const auto s : layer_names)
-    cout << "    " << s << endl;
+  for (const auto s : layer_names) {
+    const boost::shared_ptr<Layer<float> >& layer = lenet.layer_by_name(s);
+    const LayerParameter& layer_param = layer->layer_param();
+    cout << "  " << s << ": " << layer_param.type()
+         << ", bottoms " << layer_param.bottom_size()
+         << ", tops " << layer_param.top_size() << endl;
+    if (layer_param.has_convolution_param()) {
+      const ConvolutionParameter& conv_param = layer_param.convolution_param();
+      if (conv_param.has_bias_term())
+        cout << "    bias_term: " << conv_param.bias_term() << endl;
+      cout << "    pad_size: " << conv_param.pad_size() << endl;
+      cout << "    kernel_size: " << conv_param.kernel_size_size()
+           << " [" << conv_param.kernel_size(0) << "]" << endl;
+      cout << "    stride_size: " << conv_param.stride_size()
+           << " [" << conv_param.stride(0) << "]" << endl;
+    }
+
+  }
   const vector<string>& blob_names = lenet.blob_names();
   cout << "network blobs : " << blob_names.size() << endl;
   for (const auto s : blob_names)
