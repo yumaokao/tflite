862da30e61768c4afa97845cba5bcfaeec502f43
yumaokao <yumaokao@gmail.com>
2018-04-09 11:51:36 +0800

    array_extra_info: array_extra_info example for assign min/max for array entries

--- sandbox/quantor/post-quantor/offical/scripts/array_extra_info_main.py
+++ sandbox/quantor/post-quantor/offical/scripts/array_extra_info_main.py
@@ -1,8 +1,27 @@
 import array_extra_info_pb2
+import types_pb2
 
 
 def main():
-  print('Hello')
+  arrayextrainfo = array_extra_info_pb2.ArraysExtraInfo()
+  entry = arrayextrainfo.entries.add()
+  entry.name = 'YMK0'
+  entry.min = 0.0
+  entry.max = 6.0
+  entry.data_type = types_pb2.QUANTIZED_UINT8
+  entry = arrayextrainfo.entries.add()
+  entry.name = 'YMK1'
+  entry.min = -6.0
+  entry.max = 10.0
+  entry.data_type = types_pb2.QUANTIZED_UINT8
+
+  binstr = arrayextrainfo.SerializeToString()
+  print(binstr)
+  print()
+
+  arrayextrainfo_r = array_extra_info_pb2.ArraysExtraInfo()
+  arrayextrainfo_r.ParseFromString(binstr)
+  print(arrayextrainfo_r)
 
 
 if __name__ == '__main__':
