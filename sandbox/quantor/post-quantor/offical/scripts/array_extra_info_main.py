cdeb755c8966213ca2d8524cbccf49d2e9e295f4
yumaokao <yumaokao@gmail.com>
2018-04-09 13:37:44 +0800

    array_extra_info: add file write out

--- sandbox/quantor/post-quantor/offical/scripts/array_extra_info_main.py
+++ sandbox/quantor/post-quantor/offical/scripts/array_extra_info_main.py
@@ -5,22 +5,28 @@ import types_pb2
 def main():
   arrayextrainfo = array_extra_info_pb2.ArraysExtraInfo()
   entry = arrayextrainfo.entries.add()
-  entry.name = 'YMK0'
+  entry.name = 'array0'
   entry.min = 0.0
   entry.max = 6.0
   entry.data_type = types_pb2.QUANTIZED_UINT8
   entry = arrayextrainfo.entries.add()
-  entry.name = 'YMK1'
+  entry.name = 'array1'
   entry.min = -6.0
   entry.max = 10.0
   entry.data_type = types_pb2.QUANTIZED_UINT8
 
-  binstr = arrayextrainfo.SerializeToString()
-  print(binstr)
-  print()
+  # binstr = arrayextrainfo.SerializeToString()
+  # print(binstr)
+  # print()
+
+  with open('array_extra_info.pb', 'wb') as f:
+      f.write(arrayextrainfo.SerializeToString())
 
   arrayextrainfo_r = array_extra_info_pb2.ArraysExtraInfo()
-  arrayextrainfo_r.ParseFromString(binstr)
+  with open('array_extra_info.pb', 'rb') as f:
+    arrayextrainfo_r.ParseFromString(f.read())
+
+  # arrayextrainfo_r.ParseFromString(binstr)
   print(arrayextrainfo_r)
 
 
